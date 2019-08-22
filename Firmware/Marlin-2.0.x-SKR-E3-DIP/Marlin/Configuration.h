611085bc58e7fa7a3d438a43c4406f75e32be102
Wouter Snels <ofloo@h4ck.io>
2019-08-22 21:09:59 +0200

    Changed Z offset for bltouch

--- Firmware/Marlin-2.0.x-SKR-E3-DIP/Marlin/Configuration.h
+++ Firmware/Marlin-2.0.x-SKR-E3-DIP/Marlin/Configuration.h
@@ -912,7 +912,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -48  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -10  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.6   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.48   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
